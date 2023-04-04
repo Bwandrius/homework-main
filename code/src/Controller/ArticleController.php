4add0e7b37d278c4b66a6b781b1332255fe37b9b
Bwandrius <andriusborkovskij@gmail.com>
2023-04-04 12:09:18 +0300

    working on ArticleController edit() and twig

--- code/src/Controller/ArticleController.php
+++ code/src/Controller/ArticleController.php
@@ -2,11 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleFormTYpe;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
+    #[Route('/article/{id}/edit', name: 'article_edit')]
+    public function edit(Article $article, Request $request, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(ArticleFormTYpe::class, $article);
+        $form->handleRequest($request);
 
+        if($form->isSubmitted() && $form->isValid()) {
+            /** @var Article $article */
+            $article = $form->getData();
+
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success', 'Success');
+            return $this->redirectToRoute('admin_article_list');
+        }
+        return $this->render('pages/article_edit.html.twig', [
+            'articleForm' => $form->createView()
+        ]);
+    }
 }
